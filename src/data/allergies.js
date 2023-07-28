3d40752a24e8d8e07f71517c35a27867a048b1f8
zionhann <hanzion1108@gmail.com>
2023-07-28 14:04:35 +0900

    :sparkles: 타겟 알러지 및 카테고리 정보 생성

---
+++ src/data/allergies.js
@@ -0,0 +1,34 @@
+export const allergies = [
+  {
+    id: 1,
+    name: "밀",
+  },
+  {
+    id: 2,
+    name: "대두",
+  },
+  {
+    id: 3,
+    name: "호두",
+  },
+  {
+    id: 4,
+    name: "난류",
+  },
+  {
+    id: 5,
+    name: "새우",
+  },
+  {
+    id: 6,
+    name: "땅콩",
+  },
+  {
+    id: 7,
+    name: "쇠고기",
+  },
+  {
+    id: 8,
+    name: "토마토",
+  },
+];
