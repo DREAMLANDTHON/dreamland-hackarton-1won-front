b96caebcf36c9b1a4150bbca29e8b15420304215
healim01 <22000770@handong.edu>
2023-07-29 11:54:09 +0900

    rm firebase

--- src/components/Image/UploadImage.js
+++ src/components/Image/UploadImage.js
@@ -1,88 +1,88 @@
-import { useCallback, useRef, useState } from "react";
-import {
-  ref,
-  getDownloadURL,
-  uploadBytes,
-  uploadBytesResumable,
-} from "firebase/storage";
-import { storage } from "../../Firebase/firebase";
+// import { useCallback, useRef, useState } from "react";
+// import {
+//   ref,
+//   getDownloadURL,
+//   uploadBytes,
+//   uploadBytesResumable,
+// } from "firebase/storage";
+// import { storage } from "../../Firebase/firebase";
 
-import { Button, ImageListItem, ImageListItemBar } from "@mui/material";
-import ClearIcon from "@mui/icons-material/Clear";
-import StarBorderIcon from "@mui/icons-material/StarBorder";
+// import { Button, ImageListItem, ImageListItemBar } from "@mui/material";
+// import ClearIcon from "@mui/icons-material/Clear";
+// import StarBorderIcon from "@mui/icons-material/StarBorder";
 
-import compressedFile from "./compressFile";
+// import compressedFile from "./compressFile";
 
-export function ImageUpload() {
-  const [imageUrl, setImageUrl] = useState("");
-  const inputRef = useRef(null);
-  const onUploadImageButtonClick = useCallback(() => {
-    if (!inputRef.current) {
-      return;
-    }
+// export function ImageUpload() {
+//   const [imageUrl, setImageUrl] = useState("");
+//   const inputRef = useRef(null);
+//   const onUploadImageButtonClick = useCallback(() => {
+//     if (!inputRef.current) {
+//       return;
+//     }
 
-    inputRef.current.click();
-  }, []);
+//     inputRef.current.click();
+//   }, []);
 
-  const onImageChange = async (e) => {
-    e.preventDefault();
-    const file = e.target.files;
-    if (!file) return null;
+//   const onImageChange = async (e) => {
+//     e.preventDefault();
+//     const file = e.target.files;
+//     if (!file) return null;
 
-    const lowCapacityFile = await compressedFile(file[0]);
+//     const lowCapacityFile = await compressedFile(file[0]);
 
-    const storageRef = ref(storage, `files/${file[0].name}`);
-    const uploadTask = uploadBytesResumable(storageRef, lowCapacityFile);
+//     const storageRef = ref(storage, `files/${file[0].name}`);
+//     const uploadTask = uploadBytesResumable(storageRef, lowCapacityFile);
 
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        );
-      },
-      (error) => {
-        switch (error.code) {
-          case "storage/canceld":
-            alert("Upload has been canceled");
-            break;
-        }
-      },
-      () => {
-        getDownloadURL(storageRef).then((downloadURL) => {
-          console.log("File available at", downloadURL);
+//     uploadTask.on(
+//       "state_changed",
+//       (snapshot) => {
+//         const progress = Math.round(
+//           (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+//         );
+//       },
+//       (error) => {
+//         switch (error.code) {
+//           case "storage/canceld":
+//             alert("Upload has been canceled");
+//             break;
+//         }
+//       },
+//       () => {
+//         getDownloadURL(storageRef).then((downloadURL) => {
+//           console.log("File available at", downloadURL);
 
-          setImageUrl(downloadURL);
-        });
-      }
-    );
-  };
+//           setImageUrl(downloadURL);
+//         });
+//       }
+//     );
+//   };
 
-  return (
-    <>
-      <Button
-        onClick={onUploadImageButtonClick}
-        variant="outlined"
-        sx={{
-          backgroundColor: "background.default",
-          borderColor: "primary.main",
-          color: "primary.main",
-          fontWeight: "normal",
-          mt: "20px",
-        }}
-      >
-        인증샷 업로드
-      </Button>
+//   return (
+//     <>
+//       <Button
+//         onClick={onUploadImageButtonClick}
+//         variant="outlined"
+//         sx={{
+//           backgroundColor: "background.default",
+//           borderColor: "primary.main",
+//           color: "primary.main",
+//           fontWeight: "normal",
+//           mt: "20px",
+//         }}
+//       >
+//         인증샷 업로드
+//       </Button>
 
-      <input
-        hidden
-        type="file"
-        // accept="image/*"
-        accept=".jpg,.jpeg,.png,.gif,.bmp,.heic,.heif"
-        ref={inputRef}
-        onChange={onImageChange}
-      />
-      <h1>{imageUrl}</h1>
-    </>
-  );
-}
+//       <input
+//         hidden
+//         type="file"
+//         // accept="image/*"
+//         accept=".jpg,.jpeg,.png,.gif,.bmp,.heic,.heif"
+//         ref={inputRef}
+//         onChange={onImageChange}
+//       />
+//       <h1>{imageUrl}</h1>
+//     </>
+//   );
+// }
--- src/components/Image/compressFile.js
+++ src/components/Image/compressFile.js
@@ -1,23 +1,23 @@
-import imageCompression from "browser-image-compression";
+// import imageCompression from "browser-image-compression";
 
-const compressedFile = async (file) => {
-  const options = {
-    maxSizeMB: 1,
-    maxWidthOrHeight: 1920,
-    useWebWorker: true,
-  };
-  try {
-    const compressedFile = await imageCompression(file, options);
-    console.log(
-      "compressedFile instanceof Blob",
-      compressedFile instanceof Blob
-    ); // true
-    console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
+// const compressedFile = async (file) => {
+//   const options = {
+//     maxSizeMB: 1,
+//     maxWidthOrHeight: 1920,
+//     useWebWorker: true,
+//   };
+//   try {
+//     const compressedFile = await imageCompression(file, options);
+//     console.log(
+//       "compressedFile instanceof Blob",
+//       compressedFile instanceof Blob
+//     ); // true
+//     console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
 
-    return compressedFile; // write your own logic
-  } catch (error) {
-    console.log(error);
-  }
-};
+//     return compressedFile; // write your own logic
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-export default compressedFile;
+// export default compressedFile;
