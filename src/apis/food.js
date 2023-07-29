d2b644b7ca61236524e8610fb1430dac33179a2c
healim01 <22000770@handong.edu>
2023-07-29 09:47:57 +0900

    :sparkles: add apis

--- src/apis/food.js
+++ src/apis/food.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
+const baseUrl =
+  'https://apis.data.go.kr/B553748/CertImgListServiceV2/getCertImgListServiceV2';
+
 export const getFood = async (
   // name,
   name, // 품목 이름
   kind, // 품목 유형
 ) => {
-  const baseUrl =
-    'https://apis.data.go.kr/B553748/CertImgListServiceV2/getCertImgListServiceV2';
-
   let paramName = '';
   let paramKind = '';
 
@@ -21,35 +21,35 @@ export const getFood = async (
   const response = await axios.get(
     `${baseUrl}?ServiceKey=${process.env.REACT_APP_SERVICE_KEY_DECODED}${paramName}&returnType=json${paramKind}`,
   );
-  console.log('hello');
-  console.log(
-    `${baseUrl}?ServiceKey=${process.env.REACT_APP_SERVICE_KEY_DECODED}${paramName}&returnType=json${paramKind}`,
-  );
+  // console.log('hello');
+  // console.log(
+  //   `${baseUrl}?ServiceKey=${process.env.REACT_APP_SERVICE_KEY_DECODED}${paramName}&returnType=json${paramKind}`,
+  // );
 
   return response.data.body.items[0].item;
 };
 
 export const getCategorys = async (
-  // name,
   kind, // 품목 유형
+  name,
 ) => {
-  const baseUrl =
-    'https://apis.data.go.kr/B553748/CertImgListServiceV2/getCertImgListServiceV2';
-
+  let paramName = '';
   let paramKind = '';
-
+  if (name) {
+    paramName = `&prdlstNm=${encodeURIComponent(name)}`;
+  }
   if (kind) {
     paramKind = `&prdkind=${encodeURIComponent(kind)}`;
   }
 
   const response = await axios.get(
-    `${baseUrl}?ServiceKey=${process.env.REACT_APP_SERVICE_KEY_DECODED}}&returnType=json${paramKind}`,
+    `${baseUrl}?ServiceKey=${process.env.REACT_APP_SERVICE_KEY_DECODED}${paramName}&returnType=json${paramKind}`,
   );
-  console.log('bye');
-  console.log(
-    `${baseUrl}?ServiceKey=${process.env.REACT_APP_SERVICE_KEY_DECODED}&returnType=json${paramKind}`,
-  );
-  console.log('plz', response.data.body);
+  // console.log('bye');
+  // console.log(
+  //   `${baseUrl}?ServiceKey=${process.env.REACT_APP_SERVICE_KEY_DECODED}${paramName}&returnType=json${paramKind}`,
+  // );
+  // console.log('plz', response.data.body.items);
 
-  return response.data.body;
+  return response.data.body.items;
 };
